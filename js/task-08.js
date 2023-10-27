b2a3d6556aaee493b2f61beb6657eefb8e4f8793
KostiaBakharev <kostya3698@gmail.com>
2023-10-27 23:55:35 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", loginForm);
+function loginForm(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("Будь ласка, заповніть всі поля!");
+    return;
+  }
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(data);
+  form.reset();
+}
